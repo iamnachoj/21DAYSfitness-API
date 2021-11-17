b48475e74e3547557760df2b3d602d9799d613b3
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-11-17 10:03:19 +0100

    add user validators

---
+++ src/validators/user-validators.js
@@ -0,0 +1,10 @@
+import {check} from 'express-validator';
+import { isLength } from 'lodash';
+
+const name = check('name', 'Name is required.').not().isEmpty();
+const username = check('username', 'Username is required.').not().isEmpty();
+const email = check('email', 'please provide a valid email address').isEmail();
+const password = check('password', 'Password is required. Minimum 6 characters').not().isLength({min: 6})
+
+export const RegisterValidations = [password, name, username, email];
+export const AuthenteValidations = [username, password]
