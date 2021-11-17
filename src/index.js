b17ea14fbddef9af14f33af4a9d55fb048daea68
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-11-17 09:01:42 +0100

    set up server on index.js

--- src/index.js
+++ src/index.js
@@ -10,13 +10,24 @@ import {DB, PORT} from './constants';
 //initialize express application
 const app = express();
 
+// apply application middleware
+app.use(cors());
+app.use(json());
+
 // main async function 
-const main = () => {
+const main = async () => {
   try{
-     //connect with DB
-      mongoose.connect(DB, {useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true});
+      // Connect with DB
+      await mongoose.connect(DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
+      consola.success('DATABASE CONNECTED...')
+      app.listen(PORT, () => {consola.success('DATABASE CONNECTED ON PORT ' + PORT)});
      //start application listening for request on server
-  } catch (err){}
+  } catch (err){
+    consola.error('database not connected. ' + err.message)
+  }
 };
 
 main();
